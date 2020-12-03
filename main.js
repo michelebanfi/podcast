d712d39d96f34afa387826d538c511e3ec78129d
Michele Banfi <michi.banfi01@gmail.com>
2020-12-03 22:26:32 +0100

    before 3rd weeks fix

--- main.js
+++ main.js
@@ -32,12 +32,13 @@ const array = [{
     Perciò l’uomo deve disabituarsi alle cose superiori alle proprie forze, restando umile e cercando la verità in Dio. \
     Il bimbo in grembo alla madre è assimilabile al popolo nelle mani di Dio. \
     Questa è la base della religione perché noi ci affidiamo completamente nelle mani di Dio come fanno ingenuamente i bambini, \
-    stando nelle braccia delle madri e sentendosi sicuri."
+    stando nelle braccia delle madri e sentendosi sicuri.",
+    pdf: '131.pdf',
 },
 {
     mp3: 'podcastEp2.mpeg',
     title: 'Episodio 2',
-    description: 'Salmo 130',
+    description: 'Salmo 129',
 
     salmo: 'Canto delle ascensioni. \
     Dal profondo a te grido, o Signore; \
@@ -64,19 +65,20 @@ const array = [{
     Ma presso di te è il perdono e avremo il tuo timore, \
     recita il salmo che con potenza e sincerità ci indica l’unica strada per la salvezza. \
     Il timore che lui genera in noi non è scaturito da paura come fosse un re oppressivo ma dall’amore, \
-    da come lui ci guarda e come ci rincuora. il suo amore e la sua misericordia sono talmente grandi \
+    da come lui ci guarda e come ci rincuora. Il suo amore e la sua misericordia sono talmente grandi \
     per i nostri cuori che il timore è l’unica emozione che può nascere quando egli ci perdona. Diviene \
     come un senso di gratitudine, di rispetto verso colui che incondizionatamente ci ama e ciò sta alla \
     base della nostra relazione con dio. \
     \
     L'anima mia spera nella sua parola; L'anima mia attende il Signore; \
-    più che le sentinelle l'aurora. queste parole rendono bene l'idea di come siamo disperati quando abbiamo bisogno di lui.\
-    spesso l’orgoglio e i canoni della società ci vogliono tutti incrollabili e sicuri di noi, può capitare però, nelle nostre vite, \
+    più che le sentinelle l'aurora. Queste parole rendono bene l'idea di come siamo disperati quando abbiamo bisogno di lui.\
+    Spesso l’orgoglio e i canoni della società ci vogliono tutti incrollabili e sicuri di noi, può capitare però, nelle nostre vite, \
     di avere un sentimento come di sprofondare nella paura, ma per apparire sempre infallibili, \
     rimaniamo in silenzio e solo allora chiediamo aiuto a te che sei una fonte inesauribile di ascolto e \
     ci rendiamo conto che la speranza è importante.\
     Dio soffre con noi, la nostra sofferenza tocca il suo cuore. \
-    Questo è il suo modo di aiutarci: stare nel dolore con noi senza mai lasciarci soli."
+    Questo è il suo modo di aiutarci: stare nel dolore con noi senza mai lasciarci soli.",
+    pdf: '129.pdf',
 },
 ]
 
@@ -88,6 +90,7 @@ function _open(title) {
             document.getElementById('mp3').src = array[0].mp3;
             document.getElementById('title').innerHTML = array[0].title;
             document.getElementById('description').innerHTML = array[0].description;
+            document.getElementById('pdf').href = array[0].pdf;
             document.getElementById('player').style.visibility = 'visible';
             document.getElementById('player').scrollIntoView();
             break;
@@ -97,6 +100,7 @@ function _open(title) {
             document.getElementById('mp3').src = array[1].mp3;
             document.getElementById('title').innerHTML = array[1].title;
             document.getElementById('description').innerHTML = array[1].description;
+            document.getElementById('pdf').href = array[1].pdf;
             document.getElementById('player').style.visibility = 'visible';
             document.getElementById('player').scrollIntoView();
             break;

62d2292f17a4f1e01a10e24872f801835e3e6f85
Michele Banfi <michi.banfi01@gmail.com>
2020-12-14 09:18:48 +0100

    fourth episode

--- main.js
+++ main.js
@@ -1,46 +1,50 @@
 //#F9A826
 
-const array = [{
-    mp3: 'podcastEp1.mp3',
-    title: 'Episodio 1',
-    description: 'Salmo 131',
+const array = [
+  {
+    mp3: "podcastEp1.mp3",
+    title: "Episodio 1",
+    description: "Salmo 131",
 
-    salmo: 'Signore, non si inorgoglisce il mio cuore \
+    salmo:
+      "Signore, non si inorgoglisce il mio cuore \
     e non si leva con superbia il mio sguardo; \
     non vado in cerca di cose grandi, \
     superiori alle mie forze. \
     Io sono tranquillo e sereno \
     come un bimbo svezzato in braccio a sua madre, \
-    come un bimbo svezzato è l\'anima mia. \
-    Speri Israele nel Signore, ora e sempre.',
+    come un bimbo svezzato è l'anima mia. \
+    Speri Israele nel Signore, ora e sempre.",
 
-    commento: "Per comprendere meglio il Salmo è possibile \
+    commento:
+      'Per comprendere meglio il Salmo è possibile \
     suddividerlo in due differenti parti: la prima descrive le \
     nostre capacità e la seconda il modo in cui dovremmo viverle. \
     In primis sembra che vengano divise le parti del corpo a seconda di ciò che si vuole esprimere, \
-    \"non si inorgoglisce il mio cuore e non si leva con superbia il mio sguardo\”, \
+    "non si inorgoglisce il mio cuore e non si leva con superbia il mio sguardo”, \
     come se noi fossimo sempre composti da queste parti, ma sta a noi decidere cosa farne e come comportarci. \
-    L’espressione:\“Non vado in cerca di cose grandi, superiori alle mie forze. \
-    Io sono tranquillo e sereno\”, \
+    L’espressione:“Non vado in cerca di cose grandi, superiori alle mie forze. \
+    Io sono tranquillo e sereno”, \
     ci fa percepire che nella vita bisogna certamente puntare in alto, \
     ma ciò non deve essere un’ossessione quasi patologica e convulsa, \
     cercando di non raggiungere obiettivi oltre le proprie capacità. \
     Perciò nulla è troppo grande se fatto con Dio, con colui che ci dà fiducia e di cui ci possiamo pienamente fidare. \
     Non importa quanto salti in alto, salta e basta, sapendo che Dio è con te. \
-    Nella seconda parte viene proposto un parallelismo tra l’uomo e \“il bimbo”, il quale viene descritto come \“svezzato\”, \
+    Nella seconda parte viene proposto un parallelismo tra l’uomo e “il bimbo”, il quale viene descritto come “svezzato”, \
     termine che deriva dal latino e si riferisce proprio al perdere le cattive abitudini. \
     Perciò l’uomo deve disabituarsi alle cose superiori alle proprie forze, restando umile e cercando la verità in Dio. \
     Il bimbo in grembo alla madre è assimilabile al popolo nelle mani di Dio. \
     Questa è la base della religione perché noi ci affidiamo completamente nelle mani di Dio come fanno ingenuamente i bambini, \
-    stando nelle braccia delle madri e sentendosi sicuri.",
-    pdf: '131.pdf',
-},
-{
-    mp3: 'podcastEp2.mpeg',
-    title: 'Episodio 2',
-    description: 'Salmo 129',
+    stando nelle braccia delle madri e sentendosi sicuri.',
+    pdf: "131.pdf",
+  },
+  {
+    mp3: "podcastEp2.mpeg",
+    title: "Episodio 2",
+    description: "Salmo 129",
 
-    salmo: 'Canto delle ascensioni. \
+    salmo:
+      "Canto delle ascensioni. \
     Dal profondo a te grido, o Signore; \
     Signore, ascolta la mia voce. \
     Siano i tuoi orecchi attenti \
@@ -50,16 +54,17 @@ const array = [{
     Ma presso di te è il perdono: \
     e avremo il tuo timore. \
     Io spero nel Signore, \
-    l\'anima mia spera nella sua parola. \
-    L\'anima mia attende il Signore\
-    più che le sentinelle l\'aurora.\
+    l'anima mia spera nella sua parola. \
+    L'anima mia attende il Signore\
+    più che le sentinelle l'aurora.\
     Israele attenda il Signore,\
     perché presso il Signore è la misericordia \
     e grande presso di lui la redenzione. \
     Egli redimerà Israele \
-    da tutte le sue colpe. ',
+    da tutte le sue colpe. ",
 
-    commento: "Se consideri le colpe, Signore, Signore, chi potrà sussistere? \
+    commento:
+      "Se consideri le colpe, Signore, Signore, chi potrà sussistere? \
     In poche righe si definisce quanto l’amore di dio vada oltre i nostri sbagli; \
     Dio non guarda alle nostre colpe, non guarda a quanto sappiamo perfomare, non ci giudica. \
     Ma presso di te è il perdono e avremo il tuo timore, \
@@ -78,14 +83,15 @@ const array = [{
     ci rendiamo conto che la speranza è importante.\
     Dio soffre con noi, la nostra sofferenza tocca il suo cuore. \
     Questo è il suo modo di aiutarci: stare nel dolore con noi senza mai lasciarci soli.",
-    pdf: '129.pdf',
-},
-{
-    mp3: 'podcastEp3.mp3',
-    title: 'Episodio 3',
-    description: 'Salmo 22',
+    pdf: "129.pdf",
+  },
+  {
+    mp3: "podcastEp3.mp3",
+    title: "Episodio 3",
+    description: "Salmo 22",
 
-    salmo: 'Al maestro del coro. Sull\'aria: «Cerva dell\'aurora». \
+    salmo:
+      "Al maestro del coro. Sull'aria: «Cerva dell'aurora». \
     Salmo. Di Davide. \
     «Dio mio, Dio mio, perché mi hai abbandonato? \
     Tu sei lontano dalla mia salvezza»: \
@@ -109,7 +115,7 @@ const array = [{
     Al mio nascere tu mi hai raccolto,\
     dal grembo di mia madre sei tu il mio Dio.\
     Da me non stare lontano,\
-    poiché l\'angoscia è vicina\
+    poiché l'angoscia è vicina\
     e nessuno mi aiuta.\
     Mi circondano tori numerosi,\
     mi assediano tori di Basan.\
@@ -136,14 +142,14 @@ const array = [{
     Salvami dalla bocca del leone\
     e dalle corna dei bufali.\
     Annunzierò il tuo nome ai miei fratelli,\
-    ti loderò in mezzo all\'assemblea.\
+    ti loderò in mezzo all'assemblea.\
     Lodate il Signore, voi che lo temete,\
     gli dia gloria la stirpe di Giacobbe,\
     lo tema tutta la stirpe di Israele;\
     perché egli non ha disprezzato\
-    né sdegnato l\afflizione del misero,\
+    né sdegnato lafflizione del misero,\
     non gli ha nascosto il suo volto,\
-    ma, al suo grido d\'aiuto, lo ha esaudito.\
+    ma, al suo grido d'aiuto, lo ha esaudito.\
     Sei tu la mia lode nella grande assemblea,\
     scioglierò i miei voti davanti ai suoi fedeli.\
     I poveri mangeranno e saranno saziati,\
@@ -163,52 +169,126 @@ const array = [{
     Si parlerà del Signore alla generazione che viene;\
     annunzieranno la sua giustizia;\
     al popolo che nascerà diranno:\
-    «Ecco l\'opera del Signore!».'
-    ,
-
-    commento: "Nel salmo c’è la ricerca di un Dio che pare non esista.\
+    «Ecco l'opera del Signore!».",
+    commento:
+      "Nel salmo c’è la ricerca di un Dio che pare non esista.\
     Il salmo però continua dicendo che l’uomo, nel dolore ma anche nella gioia sente il bisogno di cercare questo Dio, che a volte viene trascurato e lasciato nel vuoto.\
-    \“Dio mio, Dio mio, perché mi hai abbandonato?\” è il verso con cui si apre questo salmo.\
+    “Dio mio, Dio mio, perché mi hai abbandonato?” è il verso con cui si apre questo salmo.\
     Sicuramente questa domanda è scatenata dal dolore, tanto forte e potente che porta l’uomo a credere che Dio se ne sia andato, lasciandolo solo.\
     Questa supplica, rivolta al Signore si evolve durante il salmo in una ricerca di Dio tramite la sofferenza.\
     La ricerca non sarà però vana: l’uomo si accorge che Dio non disprezza la sua sofferenza, una rivelazione che porta gioia nell’animo, Dio non ha smesso di mostrarci il suo volto.\
     Logicamente Dio non vuole il nostro dolore, però l’uomo ha bisogno di questo sentimento perché è parte della sua natura e necessario alla sua crescita.\
-    Questa è una risposta possibile, ma secondo voi \“Se c’è Dio, perchè c’è sofferenza?\”",
-    pdf: '22.pdf',
-},
-]
+    Questa è una risposta possibile, ma secondo voi “Se c’è Dio, perchè c’è sofferenza?”",
+    pdf: "22.pdf",
+  },
+  {
+    mp3: "podcastEp4.mp4",
+    title: "Episodio 4",
+    description: "Salmo 50",
+
+    salmo:
+      "Al maestro del coro. Salmo. Di Davide.\
+    Quando venne da lui il profeta Natan dopo che aveva peccato con Betsabea.\
+    Pietà di me, o Dio, secondo la tua misericordia;\
+    nella tua grande bontà cancella il mio peccato.\
+    Lavami da tutte le mie colpe,\
+    mondami dal mio peccato.\
+    Riconosco la mia colpa,\
+    il mio peccato mi sta sempre dinanzi.\
+    Contro di te, contro te solo ho peccato,\
+    quello che è male ai tuoi occhi, io l'ho fatto;\
+    perciò sei giusto quando parli,\
+    retto nel tuo giudizio.\
+    Ecco, nella colpa sono stato generato,\
+    nel peccato mi ha concepito mia madre.\
+    Ma tu vuoi la sincerità del cuore\
+    e nell'intimo m'insegni la sapienza.\
+    Purificami con issopo e sarò mondo;\
+    lavami e sarò più bianco della neve.\
+    Fammi sentire gioia e letizia,\
+    esulteranno le ossa che hai spezzato.\
+    Distogli lo sguardo dai miei peccati,\
+    cancella tutte le mie colpe.\
+    Crea in me, o Dio, un cuore puro,\
+    rinnova in me uno spirito saldo.\
+    Non respingermi dalla tua presenza\
+    e non privarmi del tuo santo spirito.\
+    Rendimi la gioia di essere salvato,\
+    sostieni in me un animo generoso.\
+    Insegnerò agli erranti le tue vie\
+    e i peccatori a te ritorneranno.\
+    Liberami dal sangue, Dio, Dio mia salvezza,\
+    la mia lingua esalterà la tua giustizia.\
+    Signore, apri le mie labbra\
+    e la mia bocca proclami la tua lode;\
+    poiché non gradisci il sacrificio\
+    e, se offro olocausti, non li accetti.\
+    Uno spirito contrito è sacrificio a Dio,\
+    un cuore affranto e umiliato, Dio, tu non disprezzi.\
+    Nel tuo amore fa grazia a Sion,\
+    rialza le mura di Gerusalemme.\
+    Allora gradirai i sacrifici prescritti,\
+    l'olocausto e l'intera oblazione,\
+    allora immoleranno vittime sopra il tuo altare.",
+
+    commento:
+      'Questo salmo presenta in pochi versi una teologia dell\'uomo e del suo peccato realistica ma ottimistica,\
+       perché nell\'abisso della colpa la luminosa mano di Dio è tesa per offrire all\'uomo il perdono liberante che salva. \
+      "Il mio peccato mi sta sempre dinanzi", ci fa capire di come siamo deboli e pigri nello scrollarci di dosso certe vecchie brutte abitudini che sembrano ormai far parte di noi.\
+      "Riconosco la mia colpa", molte volte è questo il passo più difficile da accettare e soprattutto superare per poi proseguire con la riconciliazione.\
+      "Il mio peccato mi sta sempre dinanzi" sottintende che l\'uomo è peccatore per natura e che solo Dio può espiare le sue colpe. \
+      "Tu vuoi la sincerità  di cuore e nell\'intimo mi insegni la sapienza" ci ricorda quanto la confessione sia umile e difficile, \
+      essere sinceri con se stessi e avere il coraggio di volersi migliorare è la via stretta per un cristiano. \
+      “Purificami con issopo e sarò mondo; lavami e sarò più bianco della neve.” L’uomo desidera essere lavato da tutti i propri peccati,\
+       così da mantenersi candido in anima e successivamente nel corpo, ambendo ad “un cuore puro”. \
+      "Fammi sentire gioia e letizia, esulteranno le ossa che hai spezzato": la gioia di sapersi amati sia nel peccato, sia nell’errore, \
+      di sapere che se anche le ossa sono rotte ci sono motivi per essere felici.\
+      Quale dovrebbe essere il destino di un uomo senza la speranza della salvezza di Dio, se non un oblio di peccati e incertezze?',
+    pdf: "50.pdf",
+  },
+];
 
 function _open(title) {
-    switch (title) {
-        case 'primo':
-            document.getElementById("commento").innerHTML = array[0].commento;
-            document.getElementById("salmo").innerHTML = array[0].salmo;
-            document.getElementById('mp3').src = array[0].mp3;
-            document.getElementById('title').innerHTML = array[0].title;
-            document.getElementById('description').innerHTML = array[0].description;
-            document.getElementById('pdf').href = array[0].pdf;
-            document.getElementById('player').style.visibility = 'visible';
-            document.getElementById('player').scrollIntoView();
-            break;
-        case 'secondo':
-            document.getElementById("commento").innerHTML = array[1].commento;
-            document.getElementById("salmo").innerHTML = array[1].salmo;
-            document.getElementById('mp3').src = array[1].mp3;
-            document.getElementById('title').innerHTML = array[1].title;
-            document.getElementById('description').innerHTML = array[1].description;
-            document.getElementById('pdf').href = array[1].pdf;
-            document.getElementById('player').style.visibility = 'visible';
-            document.getElementById('player').scrollIntoView();
-            break;
-        case 'terzo':
-            document.getElementById("commento").innerHTML = array[2].commento;
-            document.getElementById("salmo").innerHTML = array[2].salmo;
-            document.getElementById('mp3').src = array[2].mp3;
-            document.getElementById('title').innerHTML = array[2].title;
-            document.getElementById('description').innerHTML = array[2].description;
-            document.getElementById('pdf').href = array[2].pdf;
-            document.getElementById('player').style.visibility = 'visible';
-            document.getElementById('player').scrollIntoView();
-            break;
-    }
+  switch (title) {
+    case "primo":
+      document.getElementById("commento").innerHTML = array[0].commento;
+      document.getElementById("salmo").innerHTML = array[0].salmo;
+      document.getElementById("mp3").src = array[0].mp3;
+      document.getElementById("title").innerHTML = array[0].title;
+      document.getElementById("description").innerHTML = array[0].description;
+      document.getElementById("pdf").href = array[0].pdf;
+      document.getElementById("player").style.visibility = "visible";
+      document.getElementById("player").scrollIntoView();
+      break;
+    case "secondo":
+      document.getElementById("commento").innerHTML = array[1].commento;
+      document.getElementById("salmo").innerHTML = array[1].salmo;
+      document.getElementById("mp3").src = array[1].mp3;
+      document.getElementById("title").innerHTML = array[1].title;
+      document.getElementById("description").innerHTML = array[1].description;
+      document.getElementById("pdf").href = array[1].pdf;
+      document.getElementById("player").style.visibility = "visible";
+      document.getElementById("player").scrollIntoView();
+      break;
+    case "terzo":
+      document.getElementById("commento").innerHTML = array[2].commento;
+      document.getElementById("salmo").innerHTML = array[2].salmo;
+      document.getElementById("mp3").src = array[2].mp3;
+      document.getElementById("title").innerHTML = array[2].title;
+      document.getElementById("description").innerHTML = array[2].description;
+      document.getElementById("pdf").href = array[2].pdf;
+      document.getElementById("player").style.visibility = "visible";
+      document.getElementById("player").scrollIntoView();
+      break;
+    case "quarto":
+      document.getElementById("commento").innerHTML = array[3].commento;
+      document.getElementById("salmo").innerHTML = array[3].salmo;
+      document.getElementById("mp3").src = array[3].mp3;
+      document.getElementById("title").innerHTML = array[3].title;
+      document.getElementById("description").innerHTML = array[3].description;
+      document.getElementById("pdf").href = array[3].pdf;
+      document.getElementById("player").style.visibility = "visible";
+      document.getElementById("player").scrollIntoView();
+      break;
+  }
 }
